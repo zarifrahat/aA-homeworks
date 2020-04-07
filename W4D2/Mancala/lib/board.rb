b7abc9474cdfb97800097b76aa1b16e31eedd813
zrahat <zarifrahat123@gmail.com>
2020-04-07 07:45:57 -0400

    more changes

--- W4D2/Mancala/lib/board.rb
+++ W4D2/Mancala/lib/board.rb
@@ -3,7 +3,8 @@ class Board
   attr_accessor :cups, :length
 
   def initialize(name1, name2)
-    @name1, @name2 = name2, name2
+    @name1 = name1
+    @name2 = name2
     @cups = Array.new(14){Array.new}
     self.place_stones
     @length = @cups.length
@@ -35,11 +36,13 @@ class Board
     temp_array = @cups[start_pos].dup
     @cups[start_pos] = []
     distribution_start_index = start_pos+1
-    
+     ending_cup_idx = distribution_start_index
     if current_player_name == @name1
       until temp_array.empty?
         next distribution_start_index += 1 if distribution_start_index == 13
-        @cups[distribution_start_index] << temp_array.pop
+        @cups[distribution_start_index % 14] << temp_array.pop
+        distribution_start_index = 0 if distribution_start_index > 13
+        ending_cup_idx = distribution_start_index
         distribution_start_index += 1
       end
     end
@@ -48,15 +51,32 @@ class Board
     if current_player_name == @name2
       until temp_array.empty?
         next distribution_start_index += 1 if distribution_start_index == 6
-        @cups[distribution_start_index] << temp_array.pop
+        @cups[distribution_start_index % 14] << temp_array.pop
+        distribution_start_index = 0 if distribution_start_index > 13
+        ending_cup_idx = distribution_start_index
         distribution_start_index += 1
       end
     end
-    distribution_start_index
+    self.render
+    
+    self.next_turn(ending_cup_idx, current_player_name)
   end
 
-  def next_turn(ending_cup_idx)
+  def next_turn(ending_cup_idx, current_player_name)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if @cups[ending_cup_idx].empty?
+      return :switch
+    elsif current_player_name == @name1
+          if (0..5).to_a.include?(ending_cup_idx)
+            return :prompt
+          end
+    elsif current_player_name == @name2
+          if (7..12).to_a.include?(ending_cup_idx)
+            return :prompt
+          end
+    elsif !@cups[ending_cup_idx].empty?
+          ending_cup_idx
+    end
   end
 
   def render
