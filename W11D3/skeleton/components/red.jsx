5d564485c6c43f557b81fb9f2848a0ec505b4c3a
zrahat <zarifrahat123@gmail.com>
2020-05-27 08:36:21 -0400

    hw

---
+++ W11D3/skeleton/components/red.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        <NavLink exact to='/red' >Red only</NavLink>
+        <NavLink to='/red/orange' >Orange</NavLink>
+        <NavLink to='/red/yellow' >Yellow</NavLink>
+
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
+      </div>
+    );
+  }
+};
+
+export default Red;
